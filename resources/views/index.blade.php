88238ea313790665f596ddeafdd1d8ded59fc49d
vk-mithlesh <mithlesh.jha@vyaparkranti.in>
2025-05-30 16:09:33 +0530

    banner_text
    Signed-off-by: vk-mithlesh <mithlesh.jha@vyaparkranti.in>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1895,7 +1895,7 @@
           <p class="cs_cta_subtitle cs_fs_18 cs_white_color">
             Denouncing pleasure and praising pain was born and will give you <br> complete great explorer of the truth the master-builder.
           </p>
-          <a href="#" class="cs_btn cs_style_1 cs_fs_18 cs_semibold">
+          <a href="{{ route('tour') }}" class="cs_btn cs_style_1 cs_fs_18 cs_semibold">
             Read More
             <svg width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M19.5866 5.69629H0.41235C0.184269 5.69629 0 5.46776 0 5.1849C0 4.90204 0.184269 4.67352 0.41235 4.67352H18.5906L16.0881 1.57004C15.927 1.37028 15.927 1.04587 16.0881 0.846109C16.2492 0.646349 16.5108 0.646349 16.6718 0.846109L19.8792 4.82374C19.9977 4.97076 20.0325 5.1897 19.9681 5.38147C19.9036 5.57164 19.7529 5.69629 19.5866 5.69629Z" fill="currentColor"/>
